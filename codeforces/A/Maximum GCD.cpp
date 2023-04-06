8a1c8c569208b08728fdfb8ef2ee987ab8609a6d
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-04-06 15:05:28 +0330

    codeforces: the solution of `Maximum GCD` added

---
+++ codeforces/A/Maximum GCD.cpp
@@ -0,0 +1,24 @@
+// Problem: A. Maximum GCD
+// Contest: Codeforces - Codeforces Round 651 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/1370/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+#include <algorithm>
+#include <array>
+#include <cstring>
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int
+main()
+{
+  int t, n;
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    cout << n / 2 << endl;
+  }
+  return 0;
+}
