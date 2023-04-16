01893ebd945e0f8c940696343797b41a192242c4
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-04-16 23:46:18 +0330

    codeforces: the solution of `A+B` added

---
+++ codeforces/A/A+B.cpp
@@ -0,0 +1,27 @@
+// Problem: A. A+B?
+// Contest: Codeforces - Codeforces Round 839 (Div. 3)
+// URL: https://codeforces.com/problemset/problem/1772/A
+// Memory Limit: 512 MB
+// Time Limit: 2000 ms
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int a, b;
+    cin >> a >> b;
+    cout << (a + b) << endl;
+  }
+
+  return 0;
+}
