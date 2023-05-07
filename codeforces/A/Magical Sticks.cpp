7a351bfa758ba7083b7d4637f4266899d0b6da68
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-05-07 21:49:42 +0330

    codeforces: the solution of `Magical Sticks` added

---
+++ codeforces/A/Magical Sticks.cpp
@@ -0,0 +1,29 @@
+// Problem: A. Magical Sticks
+// Contest: Codeforces - Codeforces Round 654 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/1371/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+int
+main()
+{
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << (n + 1) / 2 << '\n';
+  }
+
+  return 0;
+}
